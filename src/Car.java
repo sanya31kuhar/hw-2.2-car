a42a5063ef10d88662ad8b8058ec8b99ccc718a0
Alexander <kuhar@mail.ru>
2022-10-05 03:48:52 +0300

    Д/з 2.2 - Car

---
+++ src/Car.java
@@ -0,0 +1,43 @@
+public class Car {
+
+    public String brand;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int productionYear;
+    public String productionCountry;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (productionCountry == null) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+        if (Double.compare(engineVolume, 0) == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+    }
+}
+
